c77492bd29b424070fde21e589c864aada99f07e
Esra_Ay <esragedikliay@gmail.com>
2023-04-10 01:55:57 +0200

    assignment1

---
+++ class-notes/Assignments/assignment-1.js
@@ -0,0 +1,115 @@
+//? 1.SORU?????????????????????????????
+
+// const not = +prompt("Notunuzu giriniz..");
+// if (not < 0 || not > 100) {
+//   alert("Yanlis deger araliginda sayi girdiniz!");
+// } else if (not <= 25) {
+//   console.log("FF");
+// } else if (not <= 45) {
+//   console.log("DD");
+// } else if (not <= 65) {
+//   console.log("CC");
+// } else if (not <= 75) {
+//   console.log("BB");
+// } else if (not <= 90) {
+//   console.log("BA");
+// } else if (not <= 100) {
+//   console.log("AA");
+// } else alert("Yanlis deger girdiniz!");
+
+
+//? 2.SORU?????????????????????????????
+
+// const ay = prompt("Lütfen sectiginiz Ay ismini giriniz..")
+
+// let siraNumarasi;
+// switch (ay) {
+//     case "ocak":
+//         siraNumarasi = 1;
+//         break;
+//     case "subat":
+//         siraNumarasi = 2;
+//         break;
+//     case "mart":
+//         siraNumarasi = 3;
+//         break;
+//     case "nisan":
+//         siraNumarasi = 4;
+//         break;
+//     case "mayis":
+//         siraNumarasi = 5;
+//         break;
+//     case "haziran":
+//         siraNumarasi = 6;
+//         break;
+//     case "temmuz":
+//         siraNumarasi = 7;
+//         break;
+//     case "agustos":
+//         siraNumarasi = 8;
+//         break;
+//     case "eylül":
+//         siraNumarasi = 9;
+//         break;
+//     case "ekim":
+//         siraNumarasi = 10;
+//         break;
+//     case "kasim":
+//         siraNumarasi = 11;
+//         break;
+//     case "aralik":
+//         siraNumarasi = 12;
+//         break;
+
+//     default:
+//         alert("Gecersiz isim girdiniz!")
+// }
+
+// console.log(`${ay} ayi ${siraNumarasi}. siradadir`)
+
+
+//? 3.SORU???????????????????????????????????????
+
+
+// const sayi1 = +prompt("lütfen sayi giriniz..")
+// const sayi2 = +prompt("lütfen farkli bir sayi giriniz..")
+// const sayi3 = +prompt("lütfen farkli bir sayi giriniz..")
+
+// const islem = prompt("lütfen yapmak istediginiz islemi giriniz(+ / * / min / max)")
+
+// let sonuc;
+// switch (islem) {
+//     case "+":
+//         sonuc = sayi1 + sayi2 + sayi3;
+//         break;
+//     case "*":
+//         sonuc = sayi1 * sayi2 * sayi3;
+//         break;
+//     case "min":
+//         if (sayi1 < sayi2 && sayi1 < sayi3) {
+//             sonuc = sayi1
+//             break;
+//         } else if (sayi2 < sayi1 && sayi2 < sayi3) {
+//             sonuc = sayi2
+//             break;
+//         }
+//         else (sayi3 < sayi1 && sayi3 < sayi2) 
+//             sonuc = sayi3
+//            break; 
+        
+
+//     case "max":
+//         sonuc = Math.max(sayi1, sayi2,sayi3)
+//         break;
+
+//     default:
+//         alert("gecersiz islem!")
+//         break;
+// }
+
+// console.log(`Bu islemin (${islem}) sonucu: ${sonuc}`)
+
+//? 4.SORU???????????????????????????????????
+
+
+
